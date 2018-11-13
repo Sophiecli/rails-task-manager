ff07d888b607af1d02fda9ce27cc7d6c101814ee
Sophie Li <sophcli@gmail.com>
2018-11-13 16:51:39 +0800

    Completed Challenge 2 w ActiveRecord - TaskManager

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,46 @@
 class TasksController < ApplicationController
+
+  before_action :task_id, only: [:show, :edit, :update, :destroy]
+
   def index
-    @task = Task.all
+    @tasks = Task.all
   end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+    redirect_to tasks_path
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    @task.update(task_params)
+    redirect_to tasks_path
+  end
+
+  def destroy
+    @task.destroy
+    redirect_to tasks_path
+  end
+
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
+
+  def task_id
+    @task = Task.find(params[:id])
+  end
+
 end
